18bbd7bebae65d8192581b8758197386470286b6
Nicolas-Amato <117320053+Nicolas-Amato@users.noreply.github.com>
2022-11-13 17:52:19 -0300

    cambio en models, views, urls de MiApp, y cambios en htmls

--- MiApp/urls.py
+++ MiApp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from MiApp.views import mostrar_pedido, mostrar_tipo_de_postre, mostrar_tipo_de_relleno, mostrar_index, nuevo_pedido
 
 urlpatterns = [
+    path('mostrar_index/', mostrar_index),
     path('mostrar_pedido/', mostrar_pedido, name='Pedido'),
+    path('nuevo_pedido/', nuevo_pedido, name='Pedido Nuevo'),
     path('mostrar_tipo_de_postre/', mostrar_tipo_de_postre, name='Postre'),
     path('mostrar_tipo_de_relleno/', mostrar_tipo_de_relleno, name='Relleno'),
-    path('mostrar_index/', mostrar_index),
-    path('nuevo_pedido/', nuevo_pedido, name='Pedido Nuevo'),
-    ]
+]
--- MiApp/views.py
+++ MiApp/views.py
@@ -6,24 +6,21 @@ def mostrar_index(request):
 
 
 def mostrar_pedido(request):
-   pedido1 = haga_su_pedido(nombre = 'rodolfo', e_mail = 'nicolasamato@outlook.com', cantidad_de_personas = 12)
-   pedido2 = haga_su_pedido(nombre = 'maru', e_mail = 'maradelbianco@hotmail.com', cantidad_de_personas = 15)
-   return render(request, 'haga_su_pedido.html' ,{'haga_su_pedido':[pedido1, pedido2]})
-
+   pedido1 = haga_su_pedido(nombre = '', e_mail = '', cantidad_de_personas ='', nombre_torta='', relleno='', crocante='', numero_de_pisos=''),
+   return render(request, 'haga_su_pedido.html' ,{'haga_su_pedido':[pedido1]})
 
 
 def nuevo_pedido(request):
    if request.method == 'post':
-      mostrar_pedido = mostrar_pedido (nombre=request.post['nombre'], e_mai=request.post['e_mai'], cantidad_de_personas=request.post['cantidad_de_personas'])
-      mostrar_pedido.save()
+      Nuevo_pedido = haga_su_pedido(nombre=request.post['nombre'], e_mai=request.post['e_mai'], cantidad_de_personas=request.post['cantidad_de_personas'])
+      Nuevo_pedido.save()
       return render(request, 'tipo_de_postre.html')
    return render(request, 'crear_nuevo_pedido.html')
 
 
-
 def mostrar_tipo_de_postre(request):
    if request.method == 'post':
-      pedido_postre = pedido_postre (cantidad_de_personas=request.post['cantidad_de_personas'], nombre_torta=request.post['nombre_torta'], relleno=request.post['relleno'])
+      pedido_postre = tipo_de_postre(cantidad_de_personas=request.post['cantidad_de_personas'], nombre_torta=request.post['nombre_torta'], relleno=request.post['relleno'])
       pedido_postre.save()        
    return render(request, 'tipo_de_relleno.html')
 
